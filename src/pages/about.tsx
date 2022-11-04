b46b48034898655b0df9628f28ab37433d2188b4
Pierre Daniel van Zyl <pierrevanzyl777@gmail.com>
2022-11-04 16:01:15 +0200

    about page previous work experience cards bg color changed

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,7 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
-import Profile from '../../public/profilePic.jpg'
+// import Image from 'next/image'
+// import Profile from '../../public/profilePic.jpg'
 
 const About: NextPage = () => {
   return (
@@ -43,20 +43,20 @@ const About: NextPage = () => {
               </span> */}            
               <h2 className='text-2xl mt-10 text-center'>Work Experience</h2>
               <article className='lg:flex lg:gap-7 md:flex-row'>
-              <ul className="flex-auto lg:w-64 hover:animate-pulse card bg-gradient-to-r from-purple-500 to-cyan-400 p-5 rounded mt-7 mb-7">
+              <ul className="flex-auto lg:w-64 hover:animate-pulse card bg-slate-500 p-5 rounded mt-7 mb-7">
                 <li><em className='font-bold'>Zohe Enterprise</em>, Polokwane, South-Africa</li>
                 <li className='ml-2 list-disc'>Practical Labour Relations and Human Resources Management Training </li>
                 <li className='ml-2 list-disc'>IT Technician for Zohe Enterprise</li>
                 <li><em className='font-bold'>Date:</em> January 2009 - January 2011</li>
                 <li><em className='font-bold'>Details:</em> During my final year in school, I decided to start work while completing my schooling to get as much experience as possible in the “real” world.</li>
               </ul>
-              <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-gradient-to-r from-purple-500 to-cyan-400 p-5 rounded mt-7 mb-7">
+              <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-slate-500 p-5 rounded mt-7 mb-7">
                 <li><em className='font-bold'>Zohe Enterprise</em>, Polokwane, South-Africa</li>
                 <li className='ml-2 list-disc'>Labour Relations Official with Amalgamated Employers Association</li> 
                 <li><em className='font-bold'>Date:</em> January 2011 - February 2014</li> 
                 <li><em className='font-bold'>Details:</em> Whilst being the IT Technician for the above-mentioned company. I was given the opportunity to become an official with the Amalgamated Employers Association where my tasks included mediation, representation in arbitrations and consulting with a vast array of clients regarding industrial relations, a subset of Human Resources.</li>
               </ul>
-              <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-gradient-to-r from-purple-500 to-cyan-400 p-5 rounded mt-7 mb-7">
+              <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-slate-500 p-5 rounded mt-7 mb-7">
                 <li><em className='font-bold'>Zohe Enterprise</em>, Polokwane, South Africa</li>
                 <li className='ml-2 list-disc'>Head of Creativity and Innovation for Zohe Enterprise</li> 
                 <li><em className='font-bold'>Date:</em> March 2014- December 2018</li>
@@ -64,20 +64,20 @@ const About: NextPage = () => {
               </ul>
               </article>
               <article className='lg:flex lg:gap-7 md:flex-row'>
-              <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-gradient-to-r from-purple-500 to-cyan-400 p-5 rounded mt-7 mb-7">
+              <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-slate-500 p-5 rounded mt-7 mb-7">
                 <li><em className='font-bold'>Self-Employed</em>, Sedgefield, Western Cape, South Africa</li> 
                 <li className='ml-2 list-disc'>Online Virtual Assistant</li>
                 <li><em className='font-bold'>Date:</em> January  2019 - To March 2020</li>
                 <li><em className='font-bold'>Details:</em> After many years in the corporate legal sector, I found myself yearning for more creativity and decided to at last pursue a career within my passions. After careful discussions with my superiors, I stepped down with the understanding that I will remain as a virtual assistant to assist in the transition without my expertise whilst I venture into my new pursuit of software engineering full-time.</li>
               </ul>
-              <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-gradient-to-r from-purple-500 to-cyan-400 p-5 rounded mt-7 mb-7">
+              <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-slate-500 p-5 rounded mt-7 mb-7">
                 <li><em className='font-bold'>Self-Employed</em>, Sedgefield, Western Cape, South Africa</li>
                 <li className='ml-2 list-disc'>Software Engineer (REACT) Freelance</li>
                 <li><em className='font-bold'>Date:</em> April 2020- June 2021</li>
                 <li><em className='font-bold'>Details:</em> I have been a freelance software engineer developing robust applications for multiple different clients using different languages and frameworks to get the job done. However, I have spent most of my time building React applications.
                 </li>
               </ul>
-              <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-gradient-to-r from-purple-500 to-cyan-400 p-5 rounded mt-7 mb-7">
+              <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-slate-500 p-5 rounded mt-7 mb-7">
                 <li><em className='font-bold'>Avant Garde 2332</em> Contractor(Remote), Sedgefield, Western Cape, South Africa</li>
                 <li className='ml-2 list-disc'>Software Engineer (T3 Stack) Contractor </li>
                 <li><em className='font-bold'>Date:</em> July 2021 to Date </li>
