cd6ce43e4db8a9ce1545de5e149f0f5a00001cf7
Pierre Daniel van Zyl <pierrevanzyl777@gmail.com>
2022-11-07 15:57:06 +0200

    About me text color change on em elements

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -44,44 +44,44 @@ const About: NextPage = () => {
               <h2 className='text-2xl mt-10 text-center'>Work Experience</h2>
               <article className='lg:flex lg:gap-7 md:flex-row'>
               <ul className="flex-auto lg:w-64 hover:animate-pulse card bg-slate-800 p-5 rounded mt-7 mb-7">
-                <li><em className='font-bold'>Zohe Enterprise</em>, Polokwane, South-Africa</li>
+                <li><em className='font-bold text-cyan-300'>Zohe Enterprise</em>, Polokwane, South-Africa</li>
                 <li className='ml-2 list-disc'>Practical Labour Relations and Human Resources Management Training </li>
                 <li className='ml-2 list-disc'>IT Technician for Zohe Enterprise</li>
-                <li><em className='font-bold'>Date:</em> January 2009 - January 2011</li>
-                <li><em className='font-bold'>Details:</em> During my final year in school, I decided to start work while completing my schooling to get as much experience as possible in the “real” world.</li>
+                <li><em className='font-bold text-cyan-300'>Date:</em> January 2009 - January 2011</li>
+                <li><em className='font-bold text-cyan-300'>Details:</em> During my final year in school, I decided to start work while completing my schooling to get as much experience as possible in the “real” world.</li>
               </ul>
               <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-slate-800 p-5 rounded mt-7 mb-7">
-                <li><em className='font-bold'>Zohe Enterprise</em>, Polokwane, South-Africa</li>
+                <li><em className='font-bold text-cyan-300'>Zohe Enterprise</em>, Polokwane, South-Africa</li>
                 <li className='ml-2 list-disc'>Labour Relations Official with Amalgamated Employers Association</li> 
-                <li><em className='font-bold'>Date:</em> January 2011 - February 2014</li> 
-                <li><em className='font-bold'>Details:</em> Whilst being the IT Technician for the above-mentioned company. I was given the opportunity to become an official with the Amalgamated Employers Association where my tasks included mediation, representation in arbitrations and consulting with a vast array of clients regarding industrial relations, a subset of Human Resources.</li>
+                <li><em className='font-bold text-cyan-300'>Date:</em> January 2011 - February 2014</li> 
+                <li><em className='font-bold text-cyan-300'>Details:</em> Whilst being the IT Technician for the above-mentioned company. I was given the opportunity to become an official with the Amalgamated Employers Association where my tasks included mediation, representation in arbitrations and consulting with a vast array of clients regarding industrial relations, a subset of Human Resources.</li>
               </ul>
               <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-slate-800 p-5 rounded mt-7 mb-7">
-                <li><em className='font-bold'>Zohe Enterprise</em>, Polokwane, South Africa</li>
+                <li><em className='font-bold text-cyan-300'>Zohe Enterprise</em>, Polokwane, South Africa</li>
                 <li className='ml-2 list-disc'>Head of Creativity and Innovation for Zohe Enterprise</li> 
-                <li><em className='font-bold'>Date:</em> March 2014- December 2018</li>
-                <li><em className='font-bold'>Details:</em> My role as an official had me extremely busy yet I always used my creativity to find new and innovative ways to combine technology and creativity into my daily tasks. I brought forth an idea to move our company paperless and with the approval of the directors  I was given the position of Head of Creativity and Innovation alongside my duties as Labour Relations Official.</li>
+                <li><em className='font-bold text-cyan-300'>Date:</em> March 2014- December 2018</li>
+                <li><em className='font-bold text-cyan-300'>Details:</em> My role as an official had me extremely busy yet I always used my creativity to find new and innovative ways to combine technology and creativity into my daily tasks. I brought forth an idea to move our company paperless and with the approval of the directors  I was given the position of Head of Creativity and Innovation alongside my duties as Labour Relations Official.</li>
               </ul>
               </article>
               <article className='lg:flex lg:gap-7 md:flex-row'>
               <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-slate-800 p-5 rounded mt-7 mb-7">
-                <li><em className='font-bold'>Self-Employed</em>, Sedgefield, Western Cape, South Africa</li> 
+                <li><em className='font-bold text-cyan-300'>Self-Employed</em>, Sedgefield, Western Cape, South Africa</li> 
                 <li className='ml-2 list-disc'>Online Virtual Assistant</li>
-                <li><em className='font-bold'>Date:</em> January  2019 - To March 2020</li>
-                <li><em className='font-bold'>Details:</em> After many years in the corporate legal sector, I found myself yearning for more creativity and decided to at last pursue a career within my passions. After careful discussions with my superiors, I stepped down with the understanding that I will remain as a virtual assistant to assist in the transition without my expertise whilst I venture into my new pursuit of software engineering full-time.</li>
+                <li><em className='font-bold text-cyan-300'>Date:</em> January  2019 - To March 2020</li>
+                <li><em className='font-bold text-cyan-300'>Details:</em> After many years in the corporate legal sector, I found myself yearning for more creativity and decided to at last pursue a career within my passions. After careful discussions with my superiors, I stepped down with the understanding that I will remain as a virtual assistant to assist in the transition without my expertise whilst I venture into my new pursuit of software engineering full-time.</li>
               </ul>
               <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-slate-800 p-5 rounded mt-7 mb-7">
-                <li><em className='font-bold'>Self-Employed</em>, Sedgefield, Western Cape, South Africa</li>
+                <li><em className='font-bold text-cyan-300'>Self-Employed</em>, Sedgefield, Western Cape, South Africa</li>
                 <li className='ml-2 list-disc'>Software Engineer (REACT) Freelance</li>
-                <li><em className='font-bold'>Date:</em> April 2020- June 2021</li>
-                <li><em className='font-bold'>Details:</em> I have been a freelance software engineer developing robust applications for multiple different clients using different languages and frameworks to get the job done. However, I have spent most of my time building React applications.
+                <li><em className='font-bold text-cyan-300'>Date:</em> April 2020- June 2021</li>
+                <li><em className='font-bold text-cyan-300'>Details:</em> I have been a freelance software engineer developing robust applications for multiple different clients using different languages and frameworks to get the job done. However, I have spent most of my time building React applications.
                 </li>
               </ul>
               <ul className="flex-auto lg:w-64 hover:animate-pulse  card bg-slate-800 p-5 rounded mt-7 mb-7">
-                <li><em className='font-bold'>Avant Garde 2332</em> Contractor(Remote), Sedgefield, Western Cape, South Africa</li>
+                <li><em className='font-bold text-cyan-300'>Avant Garde 2332</em> Contractor(Remote), Sedgefield, Western Cape, South Africa</li>
                 <li className='ml-2 list-disc'>Software Engineer (T3 Stack) Contractor </li>
-                <li><em className='font-bold'>Date:</em> July 2021 to Date </li>
-                <li><em className='font-bold'>Details:</em> I have been a working at Avant Garde 2332 up to date working mostly in React and the T3 stack using TypeScript, tRPC, PostgresSQL, TailwindCss and Prisma</li>
+                <li><em className='font-bold text-cyan-300'>Date:</em> July 2021 to Date </li>
+                <li><em className='font-bold text-cyan-300'>Details:</em> I have been a working at Avant Garde 2332 up to date working mostly in React and the T3 stack using TypeScript, tRPC, PostgresSQL, TailwindCss and Prisma</li>
               </ul>
             </article>
         </div>
